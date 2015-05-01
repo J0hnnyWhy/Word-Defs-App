fbb92482470567d1f417f0217374bf37ffb8cd63
John Young <jayelwhy@yahoo.com>
2015-05-01 10:56:31 -0700

    add methods clear, all and save to WordDef class

--- lib/word_def.rb
+++ lib/word_def.rb
@@ -1,4 +1,7 @@
 class WordDef
+
+  @@def = []
+
   define_method(:initialize) do |defin|
     @defin = defin
   end
@@ -6,4 +9,17 @@ class WordDef
   define_method(:def) do
     @defin
   end
+
+  define_method(:save) do
+    @@def.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@def = []
+  end
+
+  define_singleton_method(:all) do
+    @@def
+  end
+  
 end
--- spec/word_def_spec.rb
+++ spec/word_def_spec.rb
@@ -3,10 +3,38 @@ require('word')
 require('word_def')
 
 describe(WordDef) do
+  before() do
+    WordDef.clear()
+  end
+
   describe("#def") do
     it("returns the definition of a specific word") do
       test_def = WordDef.new("a small thing")
       expect(test_def.def()).to(eq("a small thing"))
     end
   end
+
+  describe("#save") do
+    it("saves definition of a word") do
+      test_def = WordDef.new("a small thing")
+      test_def.save()
+      expect(WordDef.all()).to(eq([test_def]))
+    end
+  end
+
+  describe(".clear") do
+    it("clears the list of definitions") do
+      test_def = WordDef.new("hi mom")
+      test_def.save()
+      WordDef.clear()
+      expect(WordDef.all()).to(eq([]))
+    end
+  end
+
+  describe(".all") do
+    it("lists all of the definitions, start empty") do
+      expect(WordDef.all()).to(eq([]))
+    end
+  end
+
 end
